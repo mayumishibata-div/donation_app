e1cdb1c07f0fbf8c9bd59495987d0cb1b8f1f65d
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-08 16:48:23 +0900

    テストコードを書く準備

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録' do
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'nameが空だと保存できないこと' do
+      end
+      it 'nameが全角日本語でないと保存できないこと' do
+      end
+      it 'name_readingが空だと保存できないこと' do
+      end
+      it 'name_readingが全角カタカナでないと保存できないこと' do
+      end
+      it 'nicknameが空だと保存できないこと' do
+      end
+      it 'nicknameが半角でないと保存できないこと' do
+      end
+    end
+  end
 end
